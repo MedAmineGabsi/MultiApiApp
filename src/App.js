dbb5a0868f70a788e33023d3798a70a9609541ad
Amine GABSI <mamine.gabsi@gmail.com>
2022-03-14 21:02:32 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <h1>Multi API App</h1>
+      </div>
+
+      <div className="container">
+        <h2 align="center" style={{ color: "#fff" }}>
+          Welcome to the multi api app, <br /> Choose the options below:
+        </h2>
+        <div className="row">
+          <div className="capsule">
+            <button className="button green">Corona</button>
+            <button className="button red">Flags</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
