d5871eb279cb8ee65fca3d6eb25451b44aeea37b
Amine GABSI <mamine.gabsi@gmail.com>
2022-03-15 12:46:53 +0100

    adding loader

---
+++ src/components/Countries.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Countries = ({country}) => {
+  return <div>CountryCards</div>;
+};
+
+export default Countries;
--- src/pages/Flags.jsx
+++ src/pages/Flags.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Loader from "../components/Loader";
+import Countries from "../components/Countries";
+import "./Loading.css";
 
 const Flags = () => {
-  return (
-  <div><p>Hello countries</p></div>
-  );
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v2/all")
+      .then((res) => {
+        setCountries(res.data);
+        console.log(res.data);
+        setLoading(true);
+      })
+      .catch((err) => console.warn(err));
+  }, []);
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <div className="loading-row">
+          <Loader />
+        </div>
+      </div>
+    );
+  } else {
+    return <Countries country={countries} />;
+  }
 };
 
 export default Flags;
